26f30fa332cead1f250ebffed740bbcd8e3b8bf5
Hyun Seungmin <boscohyun@gmail.com>
2024-03-29 09:49:56 +0900

    introduce ColorTexturePool

---
+++ NineChronicles.Mods.PVEHelper/Pools/ColorTexturePool.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace NineChronicles.Mods.PVEHelper.Pools
+{
+    internal static class ColorTexturePool
+    {
+        private readonly struct CachedTexture
+        {
+            public readonly int Width;
+            public readonly int Height;
+            public readonly Color Color;
+            public readonly Texture2D Texture;
+
+            public CachedTexture(int width, int height, Color color, Texture2D texture)
+            {
+                Width = width;
+                Height = height;
+                Color = color;
+                Texture = texture;
+            }
+        }
+
+        private static readonly List<CachedTexture> _cachedTextures = new List<CachedTexture>();
+
+        public static Texture2D Get(Color color) => Get(1, 1, color);
+
+        public static Texture2D Get(int width, int height, Color color)
+        {
+            foreach (var cachedTexture in _cachedTextures)
+            {
+                if (cachedTexture.Width == width &&
+                    cachedTexture.Height == height &&
+                    cachedTexture.Color == color)
+                {
+                    return cachedTexture.Texture;
+                }
+            }
+
+            var texture = new Texture2D(width, height);
+            for (var x = 0; x < width; x++)
+            {
+                for (var y = 0; y < height; y++)
+                {
+                    texture.SetPixel(x, y, color);
+                }
+            }
+
+            texture.Apply();
+            _cachedTextures.Add(new CachedTexture(
+                width,
+                height,
+                color,
+                texture));
+            return texture;
+        }
+    }
+}
