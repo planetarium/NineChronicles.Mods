228cc665918f552e1f7d722a30ff09f80046809c
Atralupus <me@atralupus.com>
2024-04-04 20:02:13 +0900

    Add scripts for macos

---
+++ scripts/inject-plugins.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+dotenvPath="./.env.xml"
+projectName="NineChronicles.Mods.Athena"
+solutionDirectory=$(pwd)
+publishConfiguration="Release"
+
+function get_ninechronicles_dir_from_env() {
+    nineChroniclesDir=$(xmllint --xpath 'string(//Project/PropertyGroup/NINECHRONICLES_DIR)' "$dotenvPath")
+    if [ -z "$nineChroniclesDir" ]; then
+        echo "NINECHRONICLES_DIR not found in $dotenvPath"
+        exit 1
+    fi
+    echo $nineChroniclesDir
+}
+
+function publish_project() {
+    local projectName="$1"
+    local solutionDirectory="$2"
+    local publishConfiguration="$3"
+    local projectPath="$solutionDirectory/$projectName/$projectName.csproj"
+    local publishOutputPath="$solutionDirectory/$projectName/bin/$publishConfiguration/netstandard2.1"
+
+    dotnet publish "$projectPath" -c "$publishConfiguration" -o "$publishOutputPath"
+    local lastExitCode=$?
+    echo "dotnet publish exit code: $lastExitCode"
+    if [ $lastExitCode -ne 0 ]; then
+        echo "dotnet publish failed"
+        exit 1
+    fi
+
+    echo "Publish output path: $publishOutputPath"
+    echo $publishOutputPath
+}
+
+function copy_dll_to_plugin_dir() {
+    local sourcePath="$1"
+    local dllFile="$2"
+    local nineChroniclesDir="$3"
+    local targetPath="$nineChroniclesDir/BepInEx/plugins"
+    local sourceDllPath="$sourcePath/$dllFile"
+
+    echo "Attempting to copy from $sourceDllPath to $targetPath"
+
+    if [ ! -f "$sourceDllPath" ]; then
+        echo "$dllFile not found in $sourcePath"
+        return
+    fi
+
+    if [ ! -d "$targetPath" ]; then
+        mkdir -p "$targetPath"
+    fi
+
+    cp -f "$sourceDllPath" "$targetPath"
+    echo "$dllFile copied to $targetPath"
+}
+
+nineChroniclesDir=$(get_ninechronicles_dir_from_env)
+publishOutputPath=$(publish_project "$projectName" "$solutionDirectory" "$publishConfiguration")
+copy_dll_to_plugin_dir "$projectName/bin/$publishConfiguration/netstandard2.1" "NineChronicles.Modules.BlockSimulation.dll" "$nineChroniclesDir"
+copy_dll_to_plugin_dir "$projectName/bin/$publishConfiguration/netstandard2.1" "NineChronicles.Mods.Athena.dll" "$nineChroniclesDir"
