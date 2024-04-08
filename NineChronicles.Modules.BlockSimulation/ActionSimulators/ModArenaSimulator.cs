8856e5f558736f86026d69c3d573582cf4f38932
Atralupus <me@atralupus.com>
2024-04-08 12:19:12 +0900

    temp

--- NineChronicles.Modules.BlockSimulation/ActionSimulators/ModArenaSimulator.cs
+++ NineChronicles.Modules.BlockSimulation/ActionSimulators/ModArenaSimulator.cs
@@ -1,80 +1,124 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using Libplanet.Crypto;
-using Nekoyume.Action;
-using Nekoyume.Arena;
-using Nekoyume.Extensions;
-using Nekoyume.Model;
-using Nekoyume.Model.State;
-using Nekoyume;
-using Nekoyume.Game;
-using Nekoyume.Model.Item;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using JetBrains.Annotations;
-using Lib9c.Renderers;
-using Nekoyume;
-using Nekoyume.Action;
-using Nekoyume.Game;
-using Nekoyume.Model.EnumType;
-using Nekoyume.Model.Item;
-using Nekoyume.Model.Skill;
-using Nekoyume.Model.State;
-using Nekoyume.State;
-using UnityEngine;
+// using System;
+// using System.Collections.Generic;
+// using Libplanet.Crypto;
+// using Nekoyume.Action;
+// using Nekoyume.Arena;
+// using Nekoyume.Model;
+// using Nekoyume.Model.State;
+// using Nekoyume.Game;
+// using Nekoyume.Model.Item;
+// using System.Linq;
+// using Nekoyume.Model.EnumType;
+// using Nekoyume.State;
+// using Nekoyume.Module;
+// using Libplanet.Action;
+// using Bencodex.Types;
 
-namespace NineChronicles.Modules.BlockSimulation.ActionSimulators
-{
-    public static class ModArenaSimulator
-    {
-        public static float ExecuteArena(
-            List<Equipment> equipments,
-            TableSheets tableSheets,
-            States states,
-            Address avatarAddress,
-            List<Guid> equipmentIds,
-            List<Guid> costumeIds,
-            List<RuneState> runeStates,
-            Address enemyAvatarAddress,
-            List<Guid> enemyEquipmentIds,
-            List<Guid> enemyCostumeIds,
-            long blockIndex = 0,
-            int? randomSeed = null,
-            Action<string> onLog = null)
-        {
-            randomSeed ??= new RandomImpl(DateTime.Now.Millisecond).Next();
-            var signerAddress = states.AgentState.address;
-            var avatarState = (AvatarState)states.CurrentAvatarState.Clone();
-            var avatarState = (AvatarState)states.CurrentAvatarState.Clone();
 
-            var avatarDigest = new ArenaPlayerDigest(
-                avatarState,
-                equipmentIds,
-                costumeIds,
-                runeStates);
-            var enemyDigest = new ArenaPlayerDigest(
-                enemyAvatarState,
-                enemyEquipmentIds,
-                enemyCostumeIds,
-                enemyRuneStates);
+// namespace NineChronicles.Modules.BlockSimulation.ActionSimulators
+// {
+//     public static class ModArenaSimulator
+//     {
+//         public static float ExecuteArena(
+//             TableSheets tableSheets,
+//             States states,
+//             List<Equipment> myEquipments,
+//             Address enemyAvatarAddress,
+//             long blockIndex = 0,
+//             int? randomSeed = null,
+//             Action<string> onLog = null)
+//         {
+//             randomSeed ??= new RandomImpl(DateTime.Now.Millisecond).Next();
+//             var signerAddress = states.AgentState.address;
+//             IRandom random = new RandomFakeImpl(0, 1m);
 
-            var arenaSimulatorSheets = sheets.GetArenaSimulatorSheets();
-            for (var i = 0; i < playCount; i++)
-            {
-                var simulator = new ArenaSimulator(
-                    new RandomImpl(random.Next()),
-                    BattleArena.HpIncreasingModifier);
+//             var myAvatarState = (AvatarState)states.CurrentAvatarState.Clone();
+//             var myItemSlotState = states.CurrentItemSlotStates[BattleType.Arena];
 
-                simulator.Simulate(
-                    challenger: avatarDigest,
-                    enemy: enemyDigest,
-                    arenaSimulatorSheets,
-                    setExtraValueBuffBeforeGetBuffs: true);
-            }
+//             foreach (var equipment in myEquipments)
+//             {
+//                 if (!myAvatarState.inventory.HasNonFungibleItem(equipment.NonFungibleId))
+//                 {
+//                     myAvatarState.inventory.AddItem(equipment);
+//                 }
+//             }
 
-            return simulator.Log.Result == ArenaResult.Win;
-        }
-    }
-}
+//             myAvatarState.EquipEquipments(myEquipments.Select(e => e.NonFungibleId).ToList());
+//             var equippedCount = myAvatarState.inventory.Equipments.Count(equip => equip.equipped);
+//             onLog?.Invoke($"equippedCount: {equippedCount} / equipments.Count: {myEquipments.Count}");
+//             var myEquippedIds = myAvatarState.inventory.Equipments.Where(equip => equip.equipped).Select(equip => equip.ItemId).ToList();
+//             onLog?.Invoke(string.Join("\n", myEquippedIds));
+
+//             var enemyAvatarStateValue = Game.instance.Agent.GetAvatarStatesAsync(new[] { enemyAvatarAddress }).GetAwaiter().GetResult();
+//             var enemyAvatarState = enemyAvatarStateValue[enemyAvatarAddress];
+
+//             var enemyItemSlotStateAddress = ItemSlotState.DeriveAddress(enemyAvatarAddress, BattleType.Arena);
+//             var enemyItemSlotState = states.TryGetLegacyState(enemyItemSlotStateAddress, out List rawEnemyItemSlotState)
+//                 ? new ItemSlotState(rawEnemyItemSlotState)
+//                 : new ItemSlotState(BattleType.Arena);
+//             var enemyRuneSlotStateAddress = RuneSlotState.DeriveAddress(enemyAvatarAddress, BattleType.Arena);
+//             var enemyRuneSlotState = states.TryGetLegacyState(enemyRuneSlotStateAddress, out List enemyRawRuneSlotState)
+//                 ? new RuneSlotState(enemyRawRuneSlotState)
+//                 : new RuneSlotState(BattleType.Arena);
+
+//             var enemyRuneStates = new List<RuneState>();
+//             var enemyRuneSlotInfos = enemyRuneSlotState.GetEquippedRuneSlotInfos();
+//             foreach (var address in enemyRuneSlotInfos.Select(info => RuneState.DeriveAddress(enemyAvatarAddress, info.RuneId)))
+//             {
+//                 if (states.TryGetLegacyState(address, out List rawRuneState))
+//                 {
+//                     enemyRuneStates.Add(new RuneState(rawRuneState));
+//                 }
+//             }
+
+//             var avatarDigest = new ArenaPlayerDigest(
+//                 myAvatarState,
+//                 myEquippedIds,
+//                 myItemSlotState.Costumes,
+//                 states.RuneStates);
+//             var enemyDigest = new ArenaPlayerDigest(
+//                 enemyAvatarState,
+//                 enemyItemSlotState.Equipments,
+//                 enemyItemSlotState.Costumes,
+//                 enemyRuneStates);
+
+
+//             var modifiers = new Dictionary<Address, List<StatModifier>>
+//             {
+//                 [myAvatarAddress] = new(),
+//                 [enemyAvatarAddress] = new(),
+//             };
+//             if (collectionExist)
+//             {
+//                 var collectionSheet = sheets.GetSheet<CollectionSheet>();
+//                 foreach (var (address, state) in collectionStates)
+//                 {
+//                     var modifier = modifiers[address];
+//                     foreach (var collectionId in state.Ids)
+//                     {
+//                         modifier.AddRange(collectionSheet[collectionId].StatModifiers);
+//                     }
+//                 }
+//             }
+
+//             var arenaSimulatorSheets = tableSheets.GetArenaSimulatorSheets();
+
+//             var gameConfigState = states.GetGameConfigState();
+//             var simulator = new ArenaSimulator(
+//                 new RandomImpl(random.Next()),
+//                 BattleArena.HpIncreasingModifier
+//                 gameConfigState.ShatterStrikeMaxDamage);
+
+//             simulator.Simulate(
+//                 challenger: avatarDigest,
+//                 enemy: enemyDigest,
+//                 arenaSimulatorSheets,
+//                 modifiers[myAvatarState.address],
+//                 modifiers[enemyAvatarAddress],
+//                 tableSheets.DeBuffLimitSheet,
+//                 true);
+
+//             return simulator.Log.Result == ArenaResult.Win;
+//         }
+//     }
+// }
---
+++ scripts/inject-and-run.sh
@@ -0,0 +1,2 @@
+sh ./scripts/inject-plugins.sh
+sh ./scripts/run-9c-with-mods.sh
