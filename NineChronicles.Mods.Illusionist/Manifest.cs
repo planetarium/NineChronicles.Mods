d4f2e561e8ef8faae32a46dd57b07235d49c2576
Hyun Seungmin <boscohyun@gmail.com>
2024-10-17 17:12:32 +0900

    introduce Manifest for Illusionist

---
+++ NineChronicles.Mods.Illusionist/Manifest.cs
@@ -0,0 +1,9 @@
+namespace NineChronicles.Mods.Illusionist
+{
+    public static class Manifest
+    {
+        public const string PluginGuid = "org.ninechronicles.mods.illusionist";
+        public const string PluginName = "Illusionist";
+        public const string PluginVersion = "0.1.0";
+    }
+}
